473100cfb04f10f2ff6a592b2b7cf9dae9c7693e
anjomosquito <amosquito@ssct.edu.ph>
2024-03-18 15:06:27 +0800

    Dashboard vue

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,14 @@
+<template>
+    <img src="puremed/puremed.jpg" alt="Logo" />
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      src: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
---
+++ resources/js/Components/masterlist/Masterlist.vue
@@ -0,0 +1,3 @@
+<template>
+    this is materlist
+</template>
